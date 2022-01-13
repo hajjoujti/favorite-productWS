7e5b05fd37d3da3ad8ad743602d0605b5fbefe4e
Duvigneau Charles <charles.duvigneau@gmail.com>
2022-01-13 10:12:11 +0100

    Initial commit

---
+++ src/main/java/org/al36/favorite/orderws/OrderWsApplication.java
@@ -0,0 +1,13 @@
+package org.al36.favorite.orderws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderWsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/al36/favorite/orderws/OrderWsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.al36.favorite.orderws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

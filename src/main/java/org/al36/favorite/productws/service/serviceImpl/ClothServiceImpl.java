7517f7b3ca283467d5635e6533348f1cdebbb969
hajjoujti <80820935+hajjoujti@users.noreply.github.com>
2022-01-13 23:41:06 +0100

    Completed getAllClothes, getClotheById, and postClothe methods

--- src/main/java/org/al36/favorite/productws/service/serviceImpl/ClothServiceImpl.java
+++ src/main/java/org/al36/favorite/productws/service/serviceImpl/ClothServiceImpl.java
@@ -1,30 +1,53 @@
 package org.al36.favorite.productws.service.serviceImpl;
 
 import org.al36.favorite.productws.dto.ClothDTO;
+import org.al36.favorite.productws.entity.ClothEntity;
 import org.al36.favorite.productws.repository.ClothRepository;
 import org.al36.favorite.productws.service.ClothService;
+import org.al36.favorite.productws.utils.DTOConverter;
 import org.al36.favorite.productws.utils.EntityConverter;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClothServiceImpl implements ClothService {
 
+    private final EntityConverter entityConverter;
+
+    private final DTOConverter dtoConverter;
+
+    private final ClothRepository clothRepository;
+
+    public ClothServiceImpl(EntityConverter entityConverter,
+                            DTOConverter dtoConverter,
+                            ClothRepository clothRepository) {
+        this.entityConverter = entityConverter;
+        this.dtoConverter = dtoConverter;
+        this.clothRepository = clothRepository;
+    }
+
     @Override
     public List<ClothDTO> getAllClothes() {
-        return null;
+        List<ClothDTO> clothsDTOS = new ArrayList<>();
+        clothRepository.findAll().forEach((cloth) -> clothsDTOS.add(entityConverter.toClothDTO(cloth)));
+        System.out.println(clothsDTOS);
+        return clothsDTOS;
     }
 
     @Override
-    public ClothDTO getClotheById(Integer Id) {
-        return null;
+    public ClothDTO getClotheById(Integer id) {
+        Optional<ClothEntity> clothEntity = clothRepository.findById(id);
+        return clothEntity.map(entityConverter::toClothDTO).orElse(null);
     }
 
     @Override
-    public ClothDTO postClothe(ClothDTO cloth) {
-        return null;
+    public ClothDTO postCloth(ClothDTO clothDTO) {
+        ClothEntity clothEntity = dtoConverter.ClothEntity(clothDTO);
+        clothEntity = clothRepository.save(clothEntity);
+        return entityConverter.toClothDTO(clothEntity);
     }
 
 }

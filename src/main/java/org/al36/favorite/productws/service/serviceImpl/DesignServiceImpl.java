5eb29dc41ff22bb576e4eb3273a89e73841964ad
hajjoujti <80820935+hajjoujti@users.noreply.github.com>
2022-01-14 10:53:33 +0100

    Added attributes and constructor.

--- src/main/java/org/al36/favorite/productws/service/serviceImpl/DesignServiceImpl.java
+++ src/main/java/org/al36/favorite/productws/service/serviceImpl/DesignServiceImpl.java
@@ -1,13 +1,32 @@
 package org.al36.favorite.productws.service.serviceImpl;
 
 import org.al36.favorite.productws.dto.DesignDTO;
+import org.al36.favorite.productws.repository.DesignRepository;
 import org.al36.favorite.productws.service.DesignService;
+import org.al36.favorite.productws.utils.DTOConverter;
+import org.al36.favorite.productws.utils.EntityConverter;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class DesignServiceImpl implements DesignService {
+
+    private final EntityConverter entityConverter;
+
+    private final DTOConverter dtoConverter;
+
+    private final DesignRepository designRepository;
+
+    public DesignServiceImpl(EntityConverter entityConverter,
+                             DTOConverter dtoConverter,
+                             DesignRepository designRepository) {
+        this.entityConverter = entityConverter;
+        this.dtoConverter = dtoConverter;
+        this.designRepository = designRepository;
+    }
 
     @Override
     public List<DesignDTO> getAllDesigns() {

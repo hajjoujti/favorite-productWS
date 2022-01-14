f98510def32011cca3afa567c06f5f74b5af2a3a
hajjoujti <80820935+hajjoujti@users.noreply.github.com>
2022-01-14 13:01:21 +0100

    Added findByRefDeletionDateIsNull and findByRefDeletionDateIsNullAndProductType.

--- src/main/java/org/al36/favorite/productws/repository/ClothRepository.java
+++ src/main/java/org/al36/favorite/productws/repository/ClothRepository.java
@@ -1,12 +1,18 @@
 package org.al36.favorite.productws.repository;
 
 import org.al36.favorite.productws.entity.ClothEntity;
+import org.al36.favorite.productws.entity.ProductTypeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClothRepository extends JpaRepository<ClothEntity, Integer> {
 
     Optional<ClothEntity> findByReference(String reference);
+
+    List<ClothEntity> findByRefDeletionDateIsNull();
+
+    List<ClothEntity> findByRefDeletionDateIsNullAndProductType(ProductTypeEntity productTypeEntity);
 
 }

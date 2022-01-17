49f968425f2fed34ddfc69d8070a69c525f95ffd
hajjoujti <80820935+hajjoujti@users.noreply.github.com>
2022-01-17 10:14:15 +0100

    Used GetMapping in place of RequestMapping.

--- src/main/java/org/al36/favorite/productws/rest/ProductTypeRestOperations.java
+++ src/main/java/org/al36/favorite/productws/rest/ProductTypeRestOperations.java
@@ -2,16 +2,17 @@ package org.al36.favorite.productws.rest;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/product-types")
 public interface ProductTypeRestOperations {
 
-    @RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<Object> getAllProductTypes();
 
-    @RequestMapping(value = "/{name}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{name}", consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<Object> getProductTypeByName(@PathVariable String name);
 
 }

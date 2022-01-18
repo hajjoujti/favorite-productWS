16d21ffcf3ac6b7a5191ebf9a0badd96ec05d92e
hajjoujti <80820935+hajjoujti@users.noreply.github.com>
2022-01-18 14:38:27 +0100

    Added findByClothAndSize.

--- src/main/java/org/al36/favorite/productws/repository/StockRepository.java
+++ src/main/java/org/al36/favorite/productws/repository/StockRepository.java
@@ -1,8 +1,14 @@
 package org.al36.favorite.productws.repository;
 
+import org.al36.favorite.productws.entity.ClothEntity;
+import org.al36.favorite.productws.entity.SizeEntity;
 import org.al36.favorite.productws.entity.StockEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StockRepository extends JpaRepository<StockEntity, Integer> {
+
+    Optional<StockEntity> findByClothAndSize(ClothEntity clothEntity, SizeEntity sizeEntity);
 
 }
